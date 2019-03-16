78408830abc36f90884b6fd7f4f7123d9531ad76
4-java <4-java@ruu.kr>
2019-03-16 10:43:09 +0900

    문자열로 입력받아서 한번에 계산

---
+++ src/StringCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class StringCalculator {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("계산할 값 입력 : ");
+		
+		String value = scanner.nextLine();
+		System.out.println(value);
+		String[] values = value.split(" ");
+		
+		int first = Integer.parseInt(values[0]);
+		
+		int i = 1;
+		int result = first;
+
+		while(i < values.length) {
+			String symbol = values[i];
+			System.out.println(symbol);
+			int second = Integer.parseInt(values[i+1]);
+			result = Calculator.calculate(result, symbol, second);
+			i = i + 2;
+		}
+	}
+}
