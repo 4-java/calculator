df8f261c9df0cb2c0e7616a7806386a27973da68
4-java <4-java@ruu.kr>
2019-03-16 09:49:05 +0900

    계산기 기본기능 구현 완료

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,21 +6,34 @@ public class Calculator {
 		System.out.println("첫 번째 입력값 : ");
 		int first = scanner.nextInt();
 		System.out.println(first);
+		
+		int result = first;
+		while(true) {
+			System.out.println("사칙연산 기호 : ");
+			String symbol = scanner.next();
+			
+			if(symbol.equals("quit")) {
+				System.out.println("최종 결과값 : "+ result);
+				break;
+			}
+						
+			System.out.println("두 번째 입력값 : ");
+			int second = scanner.nextInt();
+			
 
-		String symbol = scanner.next();
-		
-		System.out.println("두 번째 입력값 : ");
-		int second = scanner.nextInt();
-		System.out.println(first);
-		
-		if("+".equals(symbol)) {
-			System.out.println(first + symbol + second + "=" + (first+second));
-		} else if("-".equals(symbol)) {
-			System.out.println(first + symbol + second + "-" + (first-second));
-		} else if("*".equals(symbol)) {
-			System.out.println(first + symbol + second + "=" + (first*second));
-		} else if("/".equals(symbol)) {
-			System.out.println(first + symbol + second + "=" + (first/second));
-		} else { System.out.println("사칙연산 기호가 아닙니다."); }
+			if("+".equals(symbol)) {
+				result = result + second;
+				System.out.println("덧셈 : " + result);
+			} else if("-".equals(symbol)) {
+				System.out.println("뺄셈 : " + result);
+				result = result - second;
+			} else if("*".equals(symbol)) {
+				result = result * second;
+				System.out.println("곱셈 : " + result);
+			} else if("/".equals(symbol)) {
+				result = result / second;
+				System.out.println("나눗셈 : " + result);
+			} else { System.out.println("사칙연산 기호가 아닙니다."); }
+		}
 	}
 }
